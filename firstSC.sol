214451ab1ee32b2788ac6256c1fb219437c0c3e0
bikash5750 <gbikash5750@gmail.com>
2024-06-25 13:20:56 +0530

    Create firstSC.sol

---
+++ firstSC.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0   
+//mean koi aona code ko copy na kare easily hame licance mil ta hai
+
+pragma solidity >=0.7.0 <0.9.0; //version
+
+contract first_contract  // unable code ko contract declare kar ke contractname dena per ta hai
+{
+  //coding
+  uint public num=5;
+}
